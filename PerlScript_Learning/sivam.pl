1c85b1b31db4eb43c8029213d5b5a8e316a9f003
DHANA_Home <DHANABALAN@WINDOWS-U7AQG4M>
2017-03-03 16:57:04 -0800

    variable name

--- PerlScript_Learning/sivam.pl
+++ PerlScript_Learning/sivam.pl
@@ -2,4 +2,17 @@
 Commented lines Dhanabalan
 =cut
 
-print 'sivasakthi'
+
+print 'sivasakthi \n sakthi';
+print "\nDhanabalan \n Venkitusamy\n";
+
+=begin
+There is an important difference in single and double quotes. 
+Only double quotes interpolate variables and special characters such as newlines \n, 
+where as single quote does not interpolate any variable or special character. 
+Check below example where we are using $a as a variable to store a value and later printing that value − 
+=cut
+
+$a = 10;
+print 'will not print $a';
+print "\nvalue of $a";
