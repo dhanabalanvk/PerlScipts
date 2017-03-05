1433737990d4f5fd91067ef0f1f2133aa48007c4
DHANA_Home <DHANABALAN@WINDOWS-U7AQG4M>
2017-03-05 15:47:41 -0800

    Array and hash value

---
+++ PerlScript_Learning/array_hash_datatype.pl
@@ -0,0 +1,22 @@
+=begin
+Array example
+=cut
+@ages = (25, 30, 40);             
+@names = ("Dhanabalan Venitusamy", "Sivam", "sakthi");
+
+print "\$ages[0] = $ages[0]\n";
+print "\$ages[1] = $ages[1]\n";
+print "\$ages[2] = $ages[2]\n";
+print "\$names[0] = $names[0]\n";
+print "\$names[1] = $names[1]\n";
+print "\$names[2] = $names[2]\n";
+
+=begin
+Hash value example
+=cut
+
+%data = ('Dhana', 45, 'sivam', 30, 'sakthi', 40);
+
+print "\$data{'Dhana'} = $data{'Dhana'}\n";
+print "\$data{'sivam'} = $data{'sivam'}\n";
+print "\$data{'sakthi'} = $data{'sakthi'}\n";
